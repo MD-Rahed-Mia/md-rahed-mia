1c017b7541888c580a660b595a3f89868c83fc3c
mdrahed2023 <mdrahed708@gmail.com>
2023-08-23 16:23:09 +0600

    first commit for uploading code on github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="leftAngel"></div>
+
+        <div className="left">
+          <h1 className="welcome">Welcome.</h1>
+          <div className="left-desc">
+            <div className="hr_row"></div>
+            <div className="content">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores, maiores provident. Voluptates qui nostrum earum
+                nobis incidunt iste.
+              </p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Tempore unde repellendus amet ullam ratione assumenda?
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <h1 className="project">Projects</h1>
+          <div className="angleSmall"></div>
+          <div className="projectList">
+            <ul>
+              <li>
+                <a href="#">Jackas style bd</a>
+              </li>
+              <li>
+                <a href="#">Bd tools limited </a>
+              </li>
+              <li>
+                <a href="#">Lanka Bangla exchange</a>
+              </li>
+              <li>
+                <a href="#">facebook bd limited</a>
+              </li>
+              <li>
+                <a href="#">pran rfl sales system</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="rightAngel"></div>
+      </div>
     </div>
   );
 }
